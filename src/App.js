8781e0d9d523760ebc9d1fcc4a2d3abc5ebfddce
VaishnaviKanipakam <kanipakamvaishnavi1998@gmail.com>
2025-12-22 15:43:35 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,47 @@
+import Header from "./componets/Header";
+import Filters from "./componets/Filters";
+import ProductCard from "./componets/ProductCard";
+import Footer from "./componets/Footer";
+import { MdKeyboardArrowLeft } from "react-icons/md";
+
+import "./App.css";
+
+const App = () => {
+  return (
+    <div className="app-contianer">
+      <Header />
+      <div className="discover-products-container">
+        <h1 className="discover-products-heading">DISCOVER OUR PRODUCTS</h1>
+        <p className="discover-products-description">
+          Lorem ipsum dolor sit amet consectetur. Amet est posuere rhoncus
+          scelerisque. Dolor integer scelerisque nibh amet mi ut elementum
+          dolor.
+        </p>
+      </div>
+
+      <div className="recomended-hide-filter-container">
+        <p className="items-count">3425 Items</p>
+        <div className="app-arrow-filter-container">
+          <MdKeyboardArrowLeft className="app-left-arrow" />
+          <p className="app-hide-filter">HIDE FILTER</p>
+        </div>
+
+        <select className="app-recomended">
+          <option value="recomended">RECOMMENDED</option>
+          <option value="newestfirst">NEWEST FIRST</option>
+          <option value="popular">POPULAR</option>
+          <option value="popular">PRICE: HIGH TO LOW</option>
+          <option value="popular">PRICE: LOW TO HIGH</option>
+        </select>
+      </div>
+
+      <div className="filters-product-card-components-container">
+        <Filters />
+        <ProductCard />
+      </div>
+      <Footer />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/componets/Filters.js
@@ -0,0 +1,147 @@
+import "../App.css";
+
+const Filters = () => {
+  return (
+    <div className="filters-container">
+      <p className="filters-name">Customizble</p>
+      <hr className="filters-hr-line" />
+      <select className="filters-select-tag">
+        <option className="filters-name" value="idealfor">
+          IDEAL FOR
+        </option>
+        <option className="filters-select-option" value="men">
+          Men
+        </option>
+        <option className="filters-select-option" value="women">
+          Women
+        </option>
+        <option className="filters-select-option" value="baby&kids">
+          Baby & Kids
+        </option>
+      </select>
+      <p className="filters-all">All</p>
+      <hr className="filters-hr-line" />
+
+      <select className="filters-select-tag">
+        <option className="filters-name" value="occasion">
+          OCCASION
+        </option>
+        <option className="filters-select-option" value="men">
+          Men
+        </option>
+        <option className="filters-select-option" value="women">
+          Women
+        </option>
+        <option className="filters-select-option" value="baby&kids">
+          Baby & Kids
+        </option>
+      </select>
+      <p className="filters-all">All</p>
+      <hr className="filters-hr-line" />
+
+      <select className="filters-select-tag">
+        <option className="filters-name" value="idealfor">
+          WORK
+        </option>
+        <option className="filters-select-option" value="men">
+          Men
+        </option>
+        <option className="filters-select-option" value="women">
+          Women
+        </option>
+        <option className="filters-select-option" value="baby&kids">
+          Baby & Kids
+        </option>
+      </select>
+      <p className="filters-all">All</p>
+      <hr className="filters-hr-line" />
+
+      <select className="filters-select-tag">
+        <option className="filters-name" value="fabric">
+          FABRIC
+        </option>
+        <option className="filters-select-option" value="men">
+          Men
+        </option>
+        <option className="filters-select-option" value="women">
+          Women
+        </option>
+        <option className="filters-select-option" value="baby&kids">
+          Baby & Kids
+        </option>
+      </select>
+      <p className="filters-all">All</p>
+      <hr className="filters-hr-line" />
+
+      <select className="filters-select-tag">
+        <option className="filters-name" value="segment">
+          SEGMENT
+        </option>
+        <option className="filters-select-option" value="men">
+          Men
+        </option>
+        <option className="filters-select-option" value="women">
+          Women
+        </option>
+        <option className="filters-select-option" value="baby&kids">
+          Baby & Kids
+        </option>
+      </select>
+      <p className="filters-all">All</p>
+      <hr className="filters-hr-line" />
+
+      <select className="filters-select-tag">
+        <option className="filters-name" value="suitablefor">
+          SUITABLE FOR
+        </option>
+        <option className="filters-select-option" value="men">
+          Men
+        </option>
+        <option className="filters-select-option" value="women">
+          Women
+        </option>
+        <option className="filters-select-option" value="baby&kids">
+          Baby & Kids
+        </option>
+      </select>
+      <p className="filters-all">All</p>
+      <hr className="filters-hr-line" />
+
+      <select className="filters-select-tag">
+        <option className="filters-name" value="rawmaterials">
+          RAW MATERIALS
+        </option>
+        <option className="filters-select-option" value="men">
+          Men
+        </option>
+        <option className="filters-select-option" value="women">
+          Women
+        </option>
+        <option className="filters-select-option" value="baby&kids">
+          Baby & Kids
+        </option>
+      </select>
+      <p className="filters-all">All</p>
+      <hr className="filters-hr-line" />
+  
+      <select className="filters-select-tag">
+        <option className="filters-name" value="pattern">
+          PATTERN
+        </option>
+        <option className="filters-select-option" value="men">
+          Men
+        </option>
+        <option className="filters-select-option" value="women">
+          Women
+        </option>
+        <option className="filters-select-option" value="baby&kids">
+          Baby & Kids
+        </option>
+      </select>
+      <p className="filters-all">All</p>
+      <hr className="filters-hr-line" />
+    </div>
+  );
+};
+
+export default Filters;
